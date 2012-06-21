1b6a0e9646b567b59ce08d53f325c4d2a4feb163
Roel van Dijk <roel@rustradio.org>
2012-06-21 19:51:11 +0200

    Minor styling.

--- hippo_taskforce.rb
+++ hippo_taskforce.rb
@@ -9,8 +9,8 @@ get '/' do
       <meta charset=utf-8 />
       <meta http-equiv="refresh" content="10;URL=http://iain.nl">
     </head>
-    <body style="background-color: #e7edf8; color: #000">
-      <div style="font-family: Helvetica, Arial, sans-serif; margin:0 auto; width:904px; text-align: center;">
+    <body style="background-color: ##eeeeef; color: #503e2b;">
+      <div style="font-size:16px; font-family: 'Lucida Grande','Lucida Sans Unicode',Helvetica,Arial,sans-serif; margin:0 auto; width:904px; text-align: center;">
         <p>You will be redirected in 10 seconds, stand by..</p>
 	<p><a href="javascript:window.location.reload()">reload for your pleasure</a></p>
         <img src="hippo#{rand(10)+1}.jpg" alt="" style="border: 2px solid #000"/>
