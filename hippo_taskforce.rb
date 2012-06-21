e4278259fc1ebde79a53d92d9aaca597e4d970eb
Roel van Dijk <roel@rustradio.org>
2012-06-21 19:47:54 +0200

    Added more images.

--- hippo_taskforce.rb
+++ hippo_taskforce.rb
@@ -7,12 +7,13 @@ get '/' do
     <head>
     <title>Hippo Task Force</title>
       <meta charset=utf-8 />
-      <meta http-equiv="refresh" content="5;URL=http://iain.nl">
+      <meta http-equiv="refresh" content="10;URL=http://iain.nl">
     </head>
-    <body style="background-color: #eee">
-      <div style="margin:0 auto; width:900px; text-align: center;">
-        <p style="font-family: Helvetica, Arial, sans-serif;">You will be redirected, stand by..</p>
-        <img src="hippo-run2.jpg" alt="Running hippo."/>
+    <body style="background-color: #e7edf8; color: #000">
+      <div style="font-family: Helvetica, Arial, sans-serif; margin:0 auto; width:904px; text-align: center;">
+        <p>You will be redirected in 10 seconds, stand by..</p>
+	<p><a href="javascript:window.location.reload()">reload for your pleasure</a></p>
+        <img src="hippo#{rand(10)+1}.jpg" alt="" style="border: 2px solid #000"/>
       </div>
     </body>
     </html>
